ad091db42fe25d59c87d4aedb94acddd66dafc13
JiFish <joe@jifish.co.uk>
2024-12-28 21:13:30 +0000

    Refactor knowlege book loot table creation

--- build_datapack.py
+++ build_datapack.py
@@ -4,6 +4,7 @@ import zipfile
 import json
 import copy
 from glob import glob
+from progress_bar import printProgressBar
 from build_loottable import buildLootTable
 
 # Use zlib if we have it
@@ -57,6 +58,67 @@ def getFileJson(filename, indent = None):
     with open(filename) as jsonFile:
         return json.dumps(json.load(jsonFile), indent=indent)
 
+def parse_recipe_file(recp):
+    """Parses a recipe file and returns its JSON data and name."""
+    name = recp.replace('\\', '/').split('/')[-1][:-5]  # Extract file name without extension
+    with open(recp) as jsonFile:
+        recp_json = json.load(jsonFile)
+    return name, recp_json
+
+def should_skip_recipe(recp_json):
+    """Determines if a recipe should be skipped based on its output and keys."""
+    if recp_json['type'] not in ['minecraft:crafting_shaped', 'minecraft:crafting_shapeless']:
+        return True
+    
+    if recp_json['result']['id'].startswith("minecraft:waxed"):
+        return True
+
+    if recp_json['type'] == 'minecraft:crafting_shaped':
+        output_item = recp_json['result']['id']
+        for items in recp_json['key'].values():
+            if isinstance(items, str):
+                items = [items]
+            if output_item in items:
+                return True
+    return False
+
+def update_loot_pool(newpool, item, name, group_name):
+    """Updates the loot pool with a new or existing group."""
+    if group_name not in newpool:
+        newitem = copy.deepcopy(item)
+        newitem["functions"][0]["components"]["minecraft:recipes"][0] = "minecraft:" + name
+        newitem["functions"][0]["components"]["minecraft:lore"][0] = f'"{group_name.replace("_", " ").title()}"'
+        newpool[group_name] = newitem
+    else:
+        newpool[group_name]["functions"][0]["components"]["minecraft:recipes"].append("minecraft:" + name)
+
+def process_recipes(recipes, item):
+    """Processes all recipes and updates the loot pool."""
+    newpool = {}
+    total_recipes = len(recipes)
+
+    for i, recp in enumerate(recipes, start=1):
+        printProgressBar(i, total_recipes, "Creating knowledge books loot table...", length=40, decimals=0)
+        name, recp_json = parse_recipe_file(recp)
+
+        if should_skip_recipe(recp_json):
+            continue
+
+        group_name = recp_json.get('group', name)
+        update_loot_pool(newpool, item, name, group_name)
+
+    return newpool
+
+def create_knowledge_books_loot_table(baseloot):
+    """Creates the knowledge books loot table."""
+    with open(baseloot) as jsonFile:
+        kb = json.load(jsonFile)
+    item = kb["pools"][0]["entries"][0]
+    recipes = glob('data_extracted/base_recipe/*.json')
+    newpool = process_recipes(recipes, item)
+    kb["pools"][0]["entries"] = list(newpool.values())
+    return kb
+
 def buildDatapack(config, loottable, version):
     indent = 2 if config['indent-output'] else None
 
@@ -109,30 +171,6 @@ def buildDatapack(config, loottable, version):
         zf.writestr('data/babel/worldgen/template_pool/lost_library_pool.json', getFileJson('data/lost_library_pool.json', indent=indent))
         zf.writestr('data/babel/worldgen/structure/lost_library.json', getFileJson('data/lost_library.json', indent=indent))
         zf.write('data/lost_library.nbt', 'data/babel/structure/lost_library.nbt')
-        with open('data/knowlege_book.json') as jsonFile:
-            kb = json.load(jsonFile)
-        item = kb["pools"][0]["entries"][0]
-        newpool = {}
-        for recp in glob('data_extracted/base_recipe/*.json'):
-            name = recp.replace('\\', '/').split('/')[-1]  # Get the file name with extension
-            name = name[:-5]  # Remove the last 5 characters (".json")
-            with open(recp) as jsonFile:
-                recp_json = json.load(jsonFile)
-            if recp_json['type'] not in ['minecraft:crafting_shaped', 'minecraft:crafting_shapeless']:
-                continue
-                
-            if 'group' in recp_json:
-                group_name = recp_json['group']
-            else:
-                group_name = name
-            
-            if group_name not in newpool:
-                newitem = copy.deepcopy(item)
-                newitem["functions"][0]["components"]["minecraft:recipes"][0] = "minecraft:" + name
-                newitem["functions"][0]["components"]["minecraft:lore"][0] = "\""+group_name.replace("_", " ").title()+"\""
-                newpool[group_name] = newitem
-            else:
-                newpool[group_name]["functions"][0]["components"]["minecraft:recipes"].append("minecraft:" + name)
-        kb["pools"][0]["entries"] = list(newpool.values())
-        zf.writestr('data/babel/loot_table/knowlege_book.json', json.dumps(kb, indent=indent))
+        knowlege_book = create_knowledge_books_loot_table('data/knowlege_book.json')
+        zf.writestr('data/babel/loot_table/knowlege_book.json', json.dumps(knowlege_book, indent=indent))
     zf.close()
